9322e456e15837be26e6d0577b5f4c279d8f45bc
Ramon Gonzalez <ramongonzalez101@gmail.com>
2024-11-30 14:56:18 -0400

    diseno inicial login

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -2,16 +2,9 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import {Poppins} from 'next/font/google' 
+
+const poppins = Poppins ({subsets:['latin'], weight:['300','400','700','900']}) 
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,9 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    <html lang="en" className={poppins.className}>
+      <body 
       >
         {children}
       </body>
