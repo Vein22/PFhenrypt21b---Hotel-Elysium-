8ebf4d56952590fd0a00aea48e02201c1a298bd1
MMelvinMora <121642949+Vein22@users.noreply.github.com>
2024-11-29 21:00:47 -0400

    Fix:para usar DB_DATABASE y habilitar SSL

--- back/src/config/data-source.ts
+++ back/src/config/data-source.ts
@@ -10,12 +10,13 @@ const config = {
   port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DATABASE,
+  database: process.env.DB_DATABASE,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   migrations: [__dirname + '/../migrations/**/*.{js,ts}'],
   synchronize: true,
   logging: false,
   dropSchema: false,
+  ssl: true,
 };
 
 export default registerAs('typeorm', () => config);
