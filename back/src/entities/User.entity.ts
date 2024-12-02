5c75822ca5fab9b2490fc4c2c8db80d9d6dbd1a3
MMelvinMora <121642949+Vein22@users.noreply.github.com>
2024-12-01 23:01:33 -0400

    Swagger. name añadido, confirmPassword elminado.

--- back/src/entities/User.entity.ts
+++ back/src/entities/User.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { v4 as uuid } from 'uuid';
 
@@ -6,25 +7,88 @@ export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({unique: true})
+
+   /**
+   * El nombre del usuario.
+   * @example John Doe
+   */
+ @ApiProperty({
+    example: 'John Doe',
+    description: 'El nombre del usuario.'
+  })
+  @Column({length: 50, nullable: false})
+  name: string;
+
+
+   /**
+   * El correo electrónico del usuario.
+   * @example user@example.com
+   */
+   @ApiProperty({
+    example: 'user@example.com',
+    description: 'El correo electrónico del usuario.'
+  })
+  @Column({length:15 ,nullable: false, unique: true})
   email: string;
 
-  @Column()
+
+    /**
+     * La contraseña debe ser dificil de descifrar.
+     * @example Strong!Pass1
+     */
+  @ApiProperty({
+    example: 'Strong!Pass1',
+    description: 'La contraseña debe ser difícil de descifrar.'
+  })
+  @Column({length: 15, nullable: false})
   password: string;
 
-  @Column()
-  confirmPassword: string;
 
-  @Column()
+    /**
+     * Número de telefono personal.
+     * @example 58 214 378 6342.
+     */
+  @ApiProperty({
+    example: '58 214 378 6342',
+    description: 'Número de telefono personal.'
+  })
+  @Column({nullable: false })
   phone: number;
 
+
+    /**
+     * Número de DNI personal.
+     * @example 28099578.
+     */
+  @ApiProperty({
+    example: '28099578',
+    description: 'Número de DNI personal.'
+  })
   @Column()
   dni: number;
 
-  @Column()
+
+    /**
+     * Pais residente.
+     * @example Argentina.
+     */
+  @ApiProperty({
+    example: 'Argentina.',
+    description: 'Pais residente.'
+  })
+  @Column({length: 50})
   country: string;
 
-  @Column()
+
+    /**
+     * Dirección de domicilio.
+     * @example SnowValley sector 8 calle 3 casa 21.
+     */
+  @ApiProperty({
+    example: 'SnowValley sector 8 calle 3 casa 21.',
+    description: 'Dirección de domicilio.'
+  })
+  @Column({length: 50})
   address: string;
 
   @CreateDateColumn()
