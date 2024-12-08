f81ddeaf0685cbc356b3fc92e7ac897ebb39a0dc
XsporosX@gmail.com <saul_751@hotmail.com>
2024-12-08 12:42:16 -0400

    ajustes para presentación

--- front/src/helpers/validateLogin.ts
+++ front/src/helpers/validateLogin.ts
@@ -2,23 +2,60 @@ import { IloginError, IloginProps } from "@/interfaces/TypesLogin";
 
 export const validateFields = (values: IloginProps): IloginError => {
 
-        const errors: IloginError = {
-        };
+    //     const errors: IloginError = {
+    //     };
 
-    // if (!errors.email) {
-    //   errors.email = 'El correo electrónico es requerido';
-    // } else 
-    if ( values.email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-      errors.email = 'El correo electrónico no es válido';
+    // // if (!errors.email) {
+    // //   errors.email = 'El correo electrónico es requerido';
+    // // } else 
+    // if ( values.email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+    //   errors.email = 'El correo electrónico no es válido';
+    // }
+
+    // // if (!errors.password) {
+    // //   errors.password = 'La contraseña es requerida';
+    // // } else 
+    // if ( values.password &&  values.password.length < 8) {
+    //   errors.password = 'La contraseña debe tener al menos 8 caracteres';
+    // }
+
+    // return errors;
+    // // setErrors(errors);
+
+
+
+    const errors: IloginError = {};
+
+  // validar el email
+  if (
+    values.email &&
+    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+  ) {
+    errors.email = "El correo electrónico no es válido";
+  }
+
+  // Validar la contraseña
+  if (!values.password) {
+    errors.password = "La contraseña es obligatoria";
+  } else {
+    if (!/^(?=.*[!@#$%^&*+-])/.test(values.password)) {
+      errors.password =
+        "La contraseña debe contener al menos un carácter especial (!@#$%^&*)";
     }
-
-    // if (!errors.password) {
-    //   errors.password = 'La contraseña es requerida';
-    // } else 
-    if ( values.password &&  values.password.length < 8) {
-      errors.password = 'La contraseña debe tener al menos 8 caracteres';
+    if (values.password.length < 8) {
+      errors.password = "La contraseña debe tener al menos 8 caracteres";
     }
-
-    return errors;
-    // setErrors(errors);
+    if (!/[0-9]/.test(values.password)) {
+      errors.password = "La contraseña debe contener al menos un número";
+    }
+    if (!/[a-z]/.test(values.password)) {
+      errors.password =
+        "La contraseña debe contener al menos una letra minúscula";
+    }
+    if (!/[A-Z]/.test(values.password)) {
+      errors.password =
+        "La contraseña debe contener al menos una letra mayúscula";
+    }
+  }
+  return errors;
   };
--- front/src/helpers/validateRegister.ts
+++ front/src/helpers/validateRegister.ts
@@ -1,57 +1,93 @@
-import { valuesTypesRegisterPrueba } from "@/interfaces/TypesRegister";
-
+import {
+  valuesTypesRegisterPrueba,
+  valuesTypesRegister,
+} from "@/interfaces/TypesRegister";
 
 export const velidateFormRegister = (form: valuesTypesRegisterPrueba) => {
-    const errors: Partial<valuesTypesRegisterPrueba> = {};
-    
-    const regexName = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
-    const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const regexPassword = /^[A-Z][a-zA-Z\d@!]{6,}$/;
-    const regexPhone = /^\+?\d{7,15}$/;
-    const regexDni = /^[A-Za-z0-9]{7,12}$/;
-    const regexAddress = /^[A-Za-z0-9\s,.-]{1,94}$/;
+  //     const errors: Partial<valuesTypesRegisterPrueba> = {};
 
-    if (!form.name.trim()) {
-        errors.name = 'El campo "Nombre" es obligatorio';
-    } else if(!regexName.test(form.name.trim())) {
-        errors.name = `El campo 'Nombre' debe contener solo letras y espacios, pero has escrito: '${form.name}'.`;
-    };
+  //     const regexName = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
+  //     const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  //     const regexPassword = /^[A-Z][a-zA-Z\d@!]{6,}$/;
+  //     const regexPhone = /^\+?\d{7,15}$/;
+  //     const regexDni = /^[A-Za-z0-9]{7,12}$/;
+  //     const regexAddress = /^[A-Za-z0-9\s,.-]{1,94}$/;
 
-    if(!form.email.trim()) {
-        errors.email = 'El correo electrónico es obligatorio';
-    } else if(!regexEmail.test(form.email.trim())) {
-        errors.email = 'Por favor, ingresa una dirección de correo electrónico válida.';
-    };
+  //     if (!form.name.trim()) {
+  //         errors.name = 'El campo "Nombre" es obligatorio';
+  //     } else if(!regexName.test(form.name.trim())) {
+  //         errors.name = `El campo 'Nombre' debe contener solo letras y espacios, pero has escrito: '${form.name}'.`;
+  //     };
 
-    if(!form.password.trim()) {
-        errors.password = 'La contraseña es obligatoria';
-    } else if(!regexPassword.test(form.password.trim())) {
-        errors.password = 'La contraseña debe cumplir con los requisitos: al menos 7 caracteres, una letra mayúscula y un número.'
-    };
+  //     if(!form.email.trim()) {
+  //         errors.email = 'El correo electrónico es obligatorio';
+  //     } else if(!regexEmail.test(form.email.trim())) {
+  //         errors.email = 'Por favor, ingresa una dirección de correo electrónico válida.';
+  //     };
 
-   if(!form.confirm_password.trim()) {
-        errors.confirm_password = 'Debe de confirmar tu contraseña';
-    } else if(form.password !== form.confirm_password) {
-        errors.confirm_password = 'Las contraseñas no coinciden';
-    };
+  //     if(!form.password.trim()) {
+  //         errors.password = 'La contraseña es obligatoria';
+  //     } else if(!regexPassword.test(form.password.trim())) {
+  //         errors.password = 'La contraseña debe cumplir con los requisitos: al menos 7 caracteres, una letra mayúscula y un número.'
+  //     };
 
-    if(!form.phone.trim()) {
-        errors.phone = 'El número de teléfono es obligatorio';
-    } else if(!regexPhone.test(form.phone.trim())) {
-        errors.phone = 'El número de teléfono debe tener entre 7 y 15 dígitos, y puede incluir un signo de más (+) al principio.';
-    };
+  //    if(!form.confirm_password.trim()) {
+  //         errors.confirm_password = 'Debe de confirmar tu contraseña';
+  //     } else if(form.password !== form.confirm_password) {
+  //         errors.confirm_password = 'Las contraseñas no coinciden';
+  //     };
 
-    if(!form.Dni.trim()) {
-        errors.Dni = 'El Dni es obligatorio';
-    } else if(!regexDni.test(form.Dni.trim())) {
-        errors.Dni = 'El DNI debe tener entre 7 y 12 caracteres alfanuméricos.';
-    };
+  //     if(!form.phone.trim()) {
+  //         errors.phone = 'El número de teléfono es obligatorio';
+  //     } else if(!regexPhone.test(form.phone.trim())) {
+  //         errors.phone = 'El número de teléfono debe tener entre 7 y 15 dígitos, y puede incluir un signo de más (+) al principio.';
+  //     };
 
-    //if(!form.address.trim()) {
-      //  errors.address = 'La dirección es obligatoria';
-    //} else if(!regexAddress.test(form.address.trim())) {
-      //  errors.address = 'Por favor, ingresa una dirección válida.';
-    //};
+  //     if(!form.Dni.trim()) {
+  //         errors.Dni = 'El Dni es obligatorio';
+  //     } else if(!regexDni.test(form.Dni.trim())) {
+  //         errors.Dni = 'El DNI debe tener entre 7 y 12 caracteres alfanuméricos.';
+  //     };
 
-    return errors;
+  //if(!form.address.trim()) {
+  //  errors.address = 'La dirección es obligatoria';
+  //} else if(!regexAddress.test(form.address.trim())) {
+  //  errors.address = 'Por favor, ingresa una dirección válida.';
+  //};
+
+  // return errors;
+
+  const errors: valuesTypesRegisterPrueba = {};
+
+  // Validar el nombre
+  if (!form.name) {
+    errors.name = "El nombre es requerido";
+  }
+
+  // Validar el correo
+  if (!form.email) {
+    errors.email = "El correo electónico es requerido";
+  }
+
+  // Validar la contraseña
+  if (!form.password) {
+    errors.password = "La contraseña es requerida";
+  }
+
+  // Validar la confirmación de la contraseña
+  if (!form.confirm_password) {
+    errors.confirm_password = "La confirmación de la contraseña es requerida";
+  }
+
+  // Validar el teléfono
+  if (!form.phone) {
+    errors.phone = "El teléfono es requerido";
+  }
+
+  // Validar el DNI
+  if (!form.Dni) {
+    errors.Dni = "El DNI es requerido";
+  }
+
+  return errors;
 };
