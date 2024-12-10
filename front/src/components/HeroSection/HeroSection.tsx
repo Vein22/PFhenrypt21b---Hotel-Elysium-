c7f6078399447f2f9fabac3aee1f13ca138eeb98
Jhon Miguel Rodríguez <130907039+jhonmirh@users.noreply.github.com>
2024-12-10 19:42:23 -0400

    Link dinámico a servicios, sabermas

--- front/src/components/HeroSection/HeroSection.tsx
+++ front/src/components/HeroSection/HeroSection.tsx
@@ -1,10 +1,16 @@
+'use client'
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+
 const HeroSection = () => {
+  const [showMessage, setShowMessage] = useState(false);
+
   return (
     <section
       className="bg-cover bg-center h-screen flex items-center justify-center text-center"
       style={{ backgroundImage: "url(/fondo3.png)" }}
     >
-      <div className="bg-black bg-opacity-50 p-8 rounded-lg">
+      <div className="bg-black bg-opacity-50 p-8 rounded-lg max-w-4xl mx-auto">
         <h1
           className="text-4xl text-white font-bold mb-4"
           style={{ textShadow: "2px 2px 4px rgba(217, 119, 6, 1)" }}
@@ -19,9 +25,26 @@ const HeroSection = () => {
           Hotel y Resort de Lujo
         </p>
 
-        <button className="bg-amber-600 text-white border border-amber-800 rounded-lg py-2 px-4 hover:bg-opacity-90">
+        <motion.button
+          className="bg-amber-600 text-white border border-amber-800 rounded-lg py-2 px-4 hover:bg-amber-700 transition-all"
+          whileHover={{ scale: 1.05 }}
+          onClick={() => setShowMessage(!showMessage)}
+        >
           Saber más
-        </button>
+        </motion.button>
+
+        {showMessage && (
+          <motion.div
+            className="mt-6 p-4 text-xl bg-white text-black rounded-lg shadow-lg border-4 border-amber-500"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 20 }}
+            transition={{ duration: 0.5 }}
+          >
+            <p>El hotel perfecto. 
+            Si buscas unas vacaciones perfectas y recuerdos inolvidables, estás en el lugar indicado. Planifiquemos una estancia a tu medida, con los mejres Servicios, Atención en Habitación, Spa, Gimnasio, Peluqueria Profesional, Desayunos Buffet, la mejor Seguridad y mucho mas.</p>
+          </motion.div>
+        )}
       </div>
     </section>
   );
