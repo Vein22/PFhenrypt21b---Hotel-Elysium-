0dec761ca8e65c8f4d49eba117fe062053f5f9f0
XsporosX@gmail.com <saul_751@hotmail.com>
2024-12-15 18:10:37 -0400

    ultimas modificaciones

--- front/src/components/HomeContainer/Home.tsx
+++ front/src/components/HomeContainer/Home.tsx
@@ -3,15 +3,16 @@ import HeroSection from "../HeroSection/HeroSection";
 import FeatureSection from "../FeatureSection/FeatureSection";
 import ServicesSection from "../ServicesSection/ServicesSection";
 import Testimonials from "../Testimonials/Testimonials";
+import CarouselComponent from "../Rooms/CarouselComponent";
+
 const HomeContainer = () => {
   return (
     <div>
-
       <HeroSection />
+      <CarouselComponent />
       <FeatureSection />
       <ServicesSection />
       <Testimonials />
-
     </div>
   );
 };
