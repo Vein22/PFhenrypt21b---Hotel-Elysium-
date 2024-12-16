20fd97e60534d27423edfb1ef424c75bae985541
XsporosX@gmail.com <saul_751@hotmail.com>
2024-12-15 22:44:47 -0400

    dashboard admin

--- front/src/app/not-found.tsx
+++ front/src/app/not-found.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-[calc(100vh-8rem)] text-center bg-White text-gray-950">
-      <h2 className="text-4xl font-bold mb-4">Página no Encontrada</h2>
+    <main className="flex flex-col items-center justify-center min-h-[calc(100vh-8rem)] text-center bg-White">
+      <h1 className="text-[2.5rem] font-bold mb-4">Página no Encontrada</h1>
       <p className="text-lg mb-6">El recurso que buscas no está disponible.</p>
       <Link href="/">
         <button className="bg-amber-600 text-white border border-amber-800 rounded-lg py-2 px-6 hover:bg-opacity-90">
