2f2ab065c7607021e9053d534bab724edfd7b586
TSilvaForte <tamara.sfok@gmail.com>
2024-12-21 19:24:18 -0300

    Actualizacion de algunos estilos

--- front/src/components/Rooms/RoomCard.tsx
+++ front/src/components/Rooms/RoomCard.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 const RoomCard: React.FC<Room> = ({ roomType, title, size, beds, rating = 0, image, price, id }) => {
   
     return (
-      <div className="w-80 h-96 bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-xl transition-shadow duration-300">
+      <div className="w-80 h-96 shadow-lg rounded-lg overflow-hidden hover:shadow-xl transition-shadow duration-300 bg-blur border-light-dark hover:-translate-y-1">
         <Link href={`/rooms/${id}`}>
 
           <div className="relative" style={{ height: "50%" }}>
@@ -17,7 +17,7 @@ const RoomCard: React.FC<Room> = ({ roomType, title, size, beds, rating = 0, ima
               layout="fill"
               objectFit="cover"
             />
-            <div className="absolute top-2 right-2 bg-tertiary text-white text-sm font-semibold py-1 px-2 rounded-lg shadow-md">
+            <div className="absolute top-2 right-2 text-sm font-semibold py-1 px-2 rounded-lg shadow-md">
               ${price} / noche
             </div>
           </div>
@@ -25,12 +25,12 @@ const RoomCard: React.FC<Room> = ({ roomType, title, size, beds, rating = 0, ima
 
           <div className="h-1/2 p-4 flex flex-col justify-between">
             <div>
-              <h2 className="text-text text-sm uppercase tracking-wide">{roomType}</h2>
-              <h1 className="text-gray-900 font-bold text-lg mt-1 truncate">{title}</h1>
-              <p className="text-text text-sm mt-2">{size}</p>
+              <h2 className=" text-sm uppercase tracking-wide">{roomType}</h2>
+              <h1 className="font-bold text-lg mt-1 truncate">{title}</h1>
+              <p className=" text-sm mt-2">{size}</p>
             </div>
             <div className="mt-4 border-t pt-4 flex items-center justify-between">
-              <p className="text-text text-sm">{beds} camas</p>
+              <p className=" text-sm">{beds} camas</p>
               <div className="flex items-center">
                 {Array.from({ length: 5 }, (_, i) => (
                   <FaStar
