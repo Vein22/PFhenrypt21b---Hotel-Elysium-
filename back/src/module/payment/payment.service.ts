71bd72fd06ac914e5f52e40fd0864090fd512133
MMelvinMora <121642949+Vein22@users.noreply.github.com>
2024-12-27 20:44:25 -0400

    PaymentStatus Notification

--- back/src/module/payment/payment.service.ts
+++ back/src/module/payment/payment.service.ts
@@ -6,14 +6,23 @@ import { ReservationRepository } from '../reservations/reservations.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Reservation } from 'src/entities/Reservation.entity';
 import { Repository } from 'typeorm';
+import { NotificationsService } from '../notifications/notifications.service';
+import { User } from 'src/entities/User.entity';
+import { Room } from 'src/entities/Room.entity';
 
 @Injectable()
 export class PaymentService {
-  @InjectRepository(Reservation)
-  private readonly reservationRepository: Repository<Reservation>
   private stripe: Stripe;
 
-  constructor() {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    @InjectRepository(Room)
+    private readonly roomRepository: Repository<Room>,
+    @InjectRepository(Reservation)
+    private readonly reservationRepository: Repository<Reservation>,
+    private readonly notificationService: NotificationsService
+  ) {
     const stripeKey = process.env.STRIPE_KEY;
     if (!stripeKey) {
       throw new Error('Stripe key is not defined in environment variables');
@@ -41,6 +50,7 @@ export class PaymentService {
       metadata: {
         reservationId, 
       },
+      //success_url: 'http://localhost:4000/payments/success?reservationId=${reservationId}' ruta para actualizar PaymentStatus
       success_url: 'http://localhost:3000/success',
       cancel_url: 'http://localhost:3000/cancel',
     });
@@ -53,6 +63,8 @@ export class PaymentService {
   
   async handlePaymentSuccess(reservationId: string) {
     const reservation = await this.reservationRepository.findOne({ where: { id: reservationId } });
+    const user = await this.userRepository.findOne({ where: { id: reservation.userId } });
+    const room = await this.roomRepository.findOne({ where: { id: reservation.roomId } });
 
     if (!reservation) {
       throw new Error('Reserva no encontrada.');
@@ -60,6 +72,8 @@ export class PaymentService {
 
     reservation.paymentStatus = PaymentStatus.PAID_ONLINE;
      await this.reservationRepository.save(reservation);
+
+     await this.notificationService.sendPaymentSuccessNotification(user, room,reservation)
     return { message: 'Pago exitoso, estado de reserva actualizado.' };
   }
 }
